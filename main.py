9bb166a431003a1a947105c7e9dac5e61a1988a9
pappasleonidas <pappasleonidas@outlook.com.gr>
2022-04-25 10:40:41 +0300

    added the apis necessary for assesment

--- main.py
+++ main.py
@@ -9,29 +9,36 @@ with open("creds/cred.txt") as f1:
         username = row[0]
         pwd = row[1]
         uri = row[2]
-print(username, pwd, uri)
 driver = GraphDatabase.driver(uri=uri, auth=(username, pwd) )
 session = driver.session()
 api = Flask(__name__ )
-@api.route("/create/<string:name>&<int:id>",methods=["GET","POST"])
-def create_node(name, id):
-    query1 = """
-        create (n:Employee{NAME:$name,ID:$id})
-        """
-    obj = {"name": name, "id": id}
-    try:
-        session.run(query1, obj)
-        return  f"employee node is created with employee name={name} and id={id}"
-    except Exception as e:
-        return str(e)
-@api.route("/display",methods=["GET","POST"])
+
+@api.route("/uncompletedTasks",methods=["GET"])
 def display_node():
     q1 ="""
-    match(n:Employee) return n.NAME as NAME ,n.ID as ID
+    Match(t:Task)
+    where t.outcome = "FAIL"
+    RETURN count(t) as count    
     """
     results = session.run(q1)
-    data = results.data()
-    return jsonify(data)
+    resultsData = results.data()
+    return jsonify(resultsData)
+
+@api.route("/distinctEngineerLvl/taskId=<string:taskId>",methods=["GET"])
+def create_node(taskId):
+    print(taskId)
+    query1 = """
+            match (t:Task{task_id:$taskId})-[r:happened]-(v:Visit)
+            return distinct(v.engineer_skill_level) AS engineer_skill_level
+            """
+    print(query1)
+    obj = {"taskId": taskId}
+    try:
+        results = session.run(query1, obj)
+        resultsData = results.data()
+        return jsonify(resultsData)
+    except Exception as e:
+        return str(e)
 
 
 if __name__=="__main__":
