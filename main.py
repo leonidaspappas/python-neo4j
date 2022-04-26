35e926f3acdbfaba1187d0e8b546b6bf1ec92026
pappasleonidas <pappasleonidas@outlook.com.gr>
2022-04-26 21:42:33 +0300

    added basic string/ code handling on the distinctEngineerLvl/taskId api in case taskId does not exist in the db

--- main.py
+++ main.py
@@ -29,17 +29,23 @@ def display_node():
 
 @api.route("/distinctEngineerLvl/taskId=<string:taskId>", methods=["GET"])
 def create_node(taskId):
-    print(taskId)
+    checkQuery = """
+            match (t:Task{task_id:$taskId})
+            return t
+            """
     query1 = """
             match (t:Task{task_id:$taskId})-[r:happened]-(v:Visit)
             return distinct(v.engineer_skill_level) AS engineer_skill_level
             """
-    print(query1)
+    print(checkQuery)
     obj = {"taskId": taskId}
     try:
+        checkRes = session.run(checkQuery, obj)
+        if (len(checkRes.data())==0):
+            return "Task id " + taskId + " does not exist in db", 404
         results = session.run(query1, obj)
         resultsData = results.data()
-        return jsonify(resultsData)
+        return jsonify(resultsData), 200
     except Exception as e:
         return str(e)
 
